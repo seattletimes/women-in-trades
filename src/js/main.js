dc5bad9b11a2de28791c2e93911da99b88b8e965
Thomas Wilburn <news@thomaswilburn.net>
2015-08-20 12:43:32 -0700

    Add fullscreen, ads, top bar
    @paigecollins Note that I formatted the text in the spreadsheet to match
    Markdown's preferences for new paragraph tags. I also split the first
    one into two, since it was way too long. If we can trim them a little
    more, all the better, but it should still fit on most screens.

--- src/js/main.js
+++ src/js/main.js
@@ -13,13 +13,31 @@ var slideChange = function(shift) {
 document.body.addEventListener("keyup", function(e) {
   console.log(e.keyCode);
   switch (e.keyCode) {
-    case 39:
-    case 32:
+    case 32: //space
+    case 34: //pagedown
+    case 39: //right
       slideChange(1);
     break;
 
-    case 37:
+    case 33: //pageup
+    case 37: //left
       slideChange(-1);
     break;
+  }
+});
+
+document.body.addEventListener("click", function(e) {
+  //slide buttons
+  if (e.target.classList.contains("change-slide")) {
+    slideChange(e.target.classList.contains("next") ? 1 : -1);
+  }
+
+  //fullscreen button
+  if (e.target.classList.contains("fullscreen")) {
+    var slideContainer = document.querySelector(".slide-container");
+    if (slideContainer.requestFullscreen) slideContainer.requestFullscreen();
+    else if (slideContainer.msRequestFullscreen) slideContainer.msRequestFullscreen();
+    else if (slideContainer.webkitRequestFullscreen) slideContainer.webkitRequestFullscreen();
+    else if (slideContainer.mozRequestFullScreen) slideContainer.mozRequestFullScreen();
   }
 });
